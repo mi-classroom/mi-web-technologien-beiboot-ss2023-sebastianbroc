1a88bfbda9315d3f7f27c1270762a0485c90aeba
Sebastian Brock <sebastian.brock@outlook.de>
2023-07-02 13:03:47 +0200

    geomarker werden gerendert, linien zwischen markern

--- arlebnispfade/src/App.vue
+++ arlebnispfade/src/App.vue
@@ -1,43 +1,40 @@
 <template>
   <div id="app">
     <div id="startscreen">
-        <div id="bg-image"></div>
-        <div id="content">
+      <div id="bg-image"></div>
+      <div id="content">
         <h1>ARlebnispfade</h1>
         <button @click="activateXR">Start</button>
         <button @click="updatePosition">test Geo</button>
-        <button @click="checkForGeoMarkers">check nearby geomarkers</button>
-        <p>{{temp}}</p>
-      </div>  
+        <button @click="checkForGeoMarkers">check geomarkers in der nähe</button>
+      </div>
     </div>
     <div id="overlay" style="display: none; z-index: 10;">
       <div id="debug-info">
         <h1>Debug-Info</h1>
         <p>Position: {{ location }}</p>
-        <p>Nächster Marker: {{ nearestMarker }}</p>  
+        <p>Nächster Marker: {{ nearestMarker }}</p>
         <p>Letzte Abfrage: {{ lastGeoUpdate }}</p>
+        <p>{{error}}</p>
+        <p>{{camera ? camera.position : null}}</p>
       </div>
       <div id="add_marker" v-if="nearestMarker != null">
-        <h2>{{ nearestMarker.name }}</h2>
+        <h2>Marker in der Nähe!</h2>
+        <h3>{{ nearestMarker.name }}</h3>
       </div>
     </div>
   </div>
 </template>
 
 <script src="../node_modules/cesium/Source/Cesium.js"></script>
+<script src="../node_modules/eruda/eruda.js"></script>
 <script>
 import * as cesium from "cesium"
 import moment from "moment"
+import {Text} from 'troika-three-text'
 
 export default {
   name: 'App',
-  head: {
-    script: [
-      {
-        type: 'text/javascript', src:'webxr-geospatial.js', async: true, body: true
-      }
-    ]
-  },
   data() {
     return {
       location: {
@@ -46,46 +43,93 @@ export default {
       },
       geoMarkers: [
         {
-          name: "Test-Marker",
-          lat: 50.96562570818945,
-          lon: 7.501961930146564
+          name: "Test-Marker Handy",
+          isRendered: false,
+          lat: 50.96562189661465,
+          lon: 7.501828788690617
+        },
+        {
+          name: "Test-Marker Mac",
+          isRendered: false,
+          lat: 50.9655816,
+          lon: 7.5018389
+        },
+        {
+          name: "Test-Marker Uni",
+          isRendered: false,
+          lat: 51.022777418103765,
+          lon: 7.561976761127407
         }
       ],
       lastGeoUpdate : null,
-      nearestMarker: null
+      nearestMarker: null,
+      lastMarkerPosition: null,
+      scene: null,
+      camera: null
     }
   },
   mounted() {
+    window.onerror = this.log;
+
     window.setInterval(() => {
-    this.updatePosition()
-    this.checkForGeoMarkers()
-  }, 5000)
+      this.updatePosition()
+      this.checkForGeoMarkers()
+    }, 5000)
   },
   methods: {
     updatePosition(){
       navigator.geolocation.getCurrentPosition((result) => {
-          this.location.lat = result.coords.latitude
-          this.location.lon = result.coords.longitude
-          this.lastGeoUpdate = moment().format("m:s");
-        })
+        this.location.lat = result.coords.latitude
+        this.location.lon = result.coords.longitude
+        this.lastGeoUpdate = moment().format("h:m:s");
+      })
     },
     checkForGeoMarkers(){
-      let noMarkersNearby = false
+      let noMarkersNearby = true
       this.geoMarkers.forEach(geoMarker => {
         let R = 6378.137; // Radius of earth in KM
         let dLat = geoMarker.lat * Math.PI / 180 - this.location.lat * Math.PI / 180;
         let dLon = geoMarker.lon * Math.PI / 180 - this.location.lon * Math.PI / 180;
         let a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(this.location.lat * Math.PI / 180) * Math.cos(geoMarker.lat * Math.PI / 180) *
-        Math.sin(dLon/2) * Math.sin(dLon/2);
+            Math.cos(this.location.lat * Math.PI / 180) * Math.cos(geoMarker.lat * Math.PI / 180) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
         let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         let d = R * c;
         this.nearestMarker = d * 1000
         if((d * 1000) <= 5 ){ // if the user is near a geomarker by 5 meters
           this.nearestMarker = geoMarker
+          noMarkersNearby = false
         }
       })
       if (noMarkersNearby) this.nearestMarker = null
+      this.renderGeoMarkers()
+    },
+    renderGeoMarkers(){
+      if(this.nearestMarker !== null && this.nearestMarker.isRendered != true){
+        this.error = "geomarker wird jetzt gerendert"
+        try {
+          const loader = new THREE.GLTFLoader();
+          let sunflower;
+          loader.load("/models/sunflower/sunflower.gltf", (gltf) => {
+            sunflower = gltf.scene;
+            let clone = sunflower.clone()
+            clone.scale.set(0.1,0.1,0.1);
+            clone.position.copy(this.camera.position)
+            sunflower.name = "geomarker"
+            sunflower.visible = true
+            //this.nearestMarker.isRendered = true
+            this.scene.add(clone)
+          });
+        } catch(e){
+          this.error = e.message
+        }
+      } else if (this.nearestMarker === null) {
+        this.scene.children.forEach(object => {
+          if(object.name == "geomarker"){
+            object.visibility = false
+          }
+        })
+      }
     },
     async activateXR(){
       document.getElementById("startscreen").style.display = "none";
@@ -93,155 +137,177 @@ export default {
       document.getElementById("app").classList.add("running");
       this.updatePosition();
 
-       // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
-        const canvas = document.createElement("canvas");
-        document.body.appendChild(canvas);
-        const gl = canvas.getContext("webgl", { xrCompatible: true });
+      // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+      const canvas = document.createElement("canvas");
+      document.body.appendChild(canvas);
+      const gl = canvas.getContext("webgl", { xrCompatible: true });
 
-        let scene = new THREE.Scene();
+      this.scene = new THREE.Scene();
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-        directionalLight.position.set(10, 15, 10);
-        scene.add(directionalLight);
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+      directionalLight.position.set(10, 15, 10);
+      this.scene.add(directionalLight);
 
-        // Set up the WebGLRenderer, which handles rendering to the session's base layer.
-        const renderer = new THREE.WebGLRenderer({
-          alpha: true,
-          preserveDrawingBuffer: true,
-          canvas: canvas,
-          context: gl,
-        });
-        renderer.autoClear = false;
+      // Set up the WebGLRenderer, which handles rendering to the session's base layer.
+      const renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        preserveDrawingBuffer: true,
+        canvas: canvas,
+        context: gl,
+      });
+      renderer.autoClear = false;
 
-        // The API directly updates the camera matrices.
-        // Disable matrix auto updates so three.js doesn't attempt
-        // to handle the matrices independently.
-        const camera = new THREE.PerspectiveCamera();
-        camera.matrixAutoUpdate = false;
+      // The API directly updates the camera matrices.
+      // Disable matrix auto updates so three.js doesn't attempt
+      // to handle the matrices independently.
+      this.camera = new THREE.PerspectiveCamera();
+      this.camera.matrixAutoUpdate = false;
 
-        // Initialize a WebXR session using "immersive-ar".
-        const session = await navigator.xr.requestSession("immersive-ar", {
-          requiredFeatures: ["hit-test"],
-          optionalFeatures: ["dom-overlay"],
-          domOverlay: {root: document.body}
-        });
-        session.updateRenderState({
-          baseLayer: new XRWebGLLayer(session, gl),
-        });
+      // Initialize a WebXR session using "immersive-ar".
+      const session = await navigator.xr.requestSession("immersive-ar", {
+        requiredFeatures: ["hit-test"],
+        optionalFeatures: ["dom-overlay"],
+        domOverlay: {root: document.body}
+      });
+      session.updateRenderState({
+        baseLayer: new XRWebGLLayer(session, gl),
+      });
 
-        // A 'local' reference space has a native origin that is located
-        // near the viewer's position at the time the session was created.
-        const referenceSpace = await session.requestReferenceSpace("local");
+      // A 'local' reference space has a native origin that is located
+      // near the viewer's position at the time the session was created.
+      const referenceSpace = await session.requestReferenceSpace("local");
 
-        // Create another XRReferenceSpace that has the viewer as the origin.
-        const viewerSpace = await session.requestReferenceSpace("viewer");
-        // Perform hit testing using the viewer as origin.
-        const hitTestSource = await session.requestHitTestSource({
-          space: viewerSpace,
-        });
+      // Create another XRReferenceSpace that has the viewer as the origin.
+      const viewerSpace = await session.requestReferenceSpace("viewer");
+      // Perform hit testing using the viewer as origin.
+      const hitTestSource = await session.requestHitTestSource({
+        space: viewerSpace,
+      });
 
-        const loader = new THREE.GLTFLoader();
-        let reticle;
-        loader.load(
-          "/models/reticle/reticle.gltf",
-          function (gltf) {
+      const loader = new THREE.GLTFLoader();
+
+      let pin;
+      loader.load("/models/pin/pin.gltf", function (gltf) {
+        pin = gltf.scene;
+        pin.scale.set(5,5,5);
+      });
+
+      let reticle;
+      loader.load("/models/reticle/reticle.gltf",
+          (gltf) => { //this has to be an arrow function, otherwise it cant access the 'this' scope
             reticle = gltf.scene;
-            scene.add(reticle);
+            this.scene.add(reticle);
           }
-        );
+      );
 
-        let pin;
-        loader.load("/models/pin/pin.gltf", function (gltf) {
-          pin = gltf.scene;
-          pin.scale.set(10,10,10);
-        });
-
-        session.addEventListener("select", (event) => {
-          if (pin) {
-            const clone = pin.clone();
-            clone.position.copy(reticle.position);
-            scene.add(clone);
-          }
-        });
-
-        /*
-        // Initialize Geospatial positioning
-        let myGeoLoc = cartographic
-        alert(myGeoLoc)
-
-        */
-
-        
-        try{
-          XRGeospatialAnchor.getDeviceCartographic()
-          XRGeospatialAnchor.getDeviceElevation().then(altitude => {
-            let geoAlt = altitude
-            XRGeospatialAnchor.createGeoAnchor(new cesium.Cartographic(50.96562995732577,7.50190944425779, geoAlt)).then(object => {
-              const clone = pin.clone();
-              clone.position.copy(object.position);
-              scene.add(clone);
-            this.log(toString(object))
-        })
-          })
-        } catch(e){
-          this.log(e.message)
+      session.addEventListener("select", (event) => {
+        if (pin) {
+          const clone = pin.clone();
+          clone.position.copy(reticle.position);
+          this.scene.add(clone);
         }
-      
-      
-        
 
-        // Create a render loop that allows us to draw on the AR view.
-        const onXRFrame = (time, frame) => {
-          // Queue up the next draw request.
-          session.requestAnimationFrame(onXRFrame);
+        // lines between objects
+        if(this.lastMarkerPosition != null){
+          const material = new THREE.LineBasicMaterial({color: 0x0000ff});
+          const points = []
+          points.push(new THREE.Vector3(this.lastMarkerPosition.x, this.lastMarkerPosition.y, this.lastMarkerPosition.z))
+          points.push(new THREE.Vector3(reticle.position.x, reticle.position.y, reticle.position.z))
+          const geometry = new THREE.BufferGeometry().setFromPoints(points)
+          const line = new THREE.Line(geometry, material)
+          this.scene.add(line)
+        }
 
-          // Bind the graphics framebuffer to the baseLayer's framebuffer
-          gl.bindFramebuffer(
+        this.lastMarkerPosition = {x: reticle.position.x, y: reticle.position.y, z: reticle.position.z}
+
+        try {
+          // Test Text in 3D
+          //this shit dont be working
+          const myText = new Text()
+          this.scene.add(myText)
+          myText.text = "Hallo Arlebnispfade!"
+          myText.fontSize = 0.2
+          myText.position.z = -2
+          myText.color = "ff0000"
+          myText.sync()
+        } catch(e){
+          this.error = e.message
+        }
+      });
+
+
+      // Create a render loop that allows us to draw on the AR view.
+      const onXRFrame = (time, frame) => {
+        // Queue up the next draw request.
+        session.requestAnimationFrame(onXRFrame);
+
+        // Bind the graphics framebuffer to the baseLayer's framebuffer
+        gl.bindFramebuffer(
             gl.FRAMEBUFFER,
             session.renderState.baseLayer.framebuffer
+        );
+
+        // Retrieve the pose of the device.
+        // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
+        const pose = frame.getViewerPose(referenceSpace);
+        if (pose) {
+          // In mobile AR, we only have one view.
+          const view = pose.views[0];
+
+          const viewport = session.renderState.baseLayer.getViewport(view);
+          renderer.setSize(
+            Math.max(
+              document.documentElement.clientWidth || 0,
+              window.innerWidth || 0
+            ),
+            Math.max(
+              document.documentElement.clientHeight || 0,
+              window.innerHeight || 0
+            )
           );
 
-          // Retrieve the pose of the device.
-          // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
-          const pose = frame.getViewerPose(referenceSpace);
-          if (pose) {
-            // In mobile AR, we only have one view.
-            const view = pose.views[0];
+          // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+          this.camera.matrix.fromArray(view.transform.matrix);
+          this.camera.projectionMatrix.fromArray(view.projectionMatrix);
+          this.camera.updateMatrixWorld(true);
 
-            const viewport = session.renderState.baseLayer.getViewport(view);
-            renderer.setSize(
-              Math.max(
-                document.documentElement.clientWidth || 0,
-                window.innerWidth || 0
-              ),
-              Math.max(
-                document.documentElement.clientHeight || 0,
-                window.innerHeight || 0
-              )
-            );
-
-            // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-            camera.matrix.fromArray(view.transform.matrix);
-            camera.projectionMatrix.fromArray(view.projectionMatrix);
-            camera.updateMatrixWorld(true);
-
-            const hitTestResults = frame.getHitTestResults(hitTestSource);
-            if (hitTestResults.length > 0 && reticle) {
-              const hitPose = hitTestResults[0].getPose(referenceSpace);
-              reticle.visible = true;
-              reticle.position.set(
+          const hitTestResults = frame.getHitTestResults(hitTestSource);
+          if (hitTestResults.length > 0 && reticle) {
+            const hitPose = hitTestResults[0].getPose(referenceSpace);
+            reticle.visible = true;
+            reticle.position.set(
                 hitPose.transform.position.x,
                 hitPose.transform.position.y,
                 hitPose.transform.position.z
-              );
-              reticle.updateMatrixWorld(true);
-            }
-
-            // Render the scene with THREE.WebGLRenderer.
-            renderer.render(scene, camera);
+            );
+            reticle.updateMatrixWorld(true);
           }
-        };
-        session.requestAnimationFrame(onXRFrame);
+
+
+          // Render the scene with THREE.WebGLRenderer.
+          renderer.render(this.scene, this.camera);
+        }
+      };
+      session.requestAnimationFrame(onXRFrame);
+    },
+    requestPermission(permission) {
+      return new Promise((resolve, reject) => {
+        navigator.permissions.query({ name: permission }).then((result) => {
+          if (result.state === "granted") {
+            console.log(`Permission ${permission} already granted`);
+            resolve();
+          } else if (result.state === "prompt") {
+            console.log(`Permission ${permission} prompt`);
+            resolve();
+          } else if (result.state === "denied") {
+            console.log(`Permission ${permission} denied`);
+            reject();
+          }
+          result.addEventListener("change", () => {
+            report(result.state);
+          });
+        });
+      })
     },
     log(message){
       const requestOptions = {
@@ -249,7 +315,7 @@ export default {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({content: message})
       };
-      fetch("http://192.168.2.140:3000/log", requestOptions)
+      fetch("http://192.168.2.140:3001/log", requestOptions)
     }
   }
 }
@@ -271,9 +337,9 @@ body {
 }
 
 #app {
-    background: rgba(10,50,10, 1);
-    height: 100vh;
-    margin: 0;
+  background: rgba(10,50,10, 1);
+  height: 100vh;
+  margin: 0;
 }
 
 #app.running {
@@ -281,21 +347,21 @@ body {
 }
 
 #app h1 {
-    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: white;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  color: white;
 }
 
 #content {
-    border-radius: 20px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-    margin: auto;
-    position: absolute;
-    top: 25%;
-    left: 25%;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  margin: auto;
+  position: absolute;
+  top: 25%;
+  left: 25%;
 }
 
 #content button {
@@ -313,10 +379,17 @@ body {
 }
 
 #add_marker {
-  background: rgba(10,50,10, 0.5);
-  width: 100%;
+  background: white;
+  border-radius: 5px 5px 0 0 ;
+  width: 100vw;
   position: absolute;
-  bottom: 0;
+  bottom: 100px;
+}
+
+#add_marker h2, #add_marker h3 {
+  font-family: sans-serif;
+  padding: 10px;
+  margin: 0;
 }
 
 #debug-info {
